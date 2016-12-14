269abb83eb02d3989e47434947f1f4f39948ff3b
Mark Briggs <briggsm@alumni.msoe.edu>
2016-12-14 12:44:41 +0300

    updated chrome.sh to new format & added saveAsFilename

--- Multi App Installer/Scripts/chrome.sh
+++ Multi App Installer/Scripts/chrome.sh
@@ -8,12 +8,12 @@ function echoUsage {
     echo "Usage: $0 [-appMeta [en|tr|ru] | -i path/to/sourceFolder/]"
 }
 
-if [ "$1" != "-appMeta" ] && [ "$1" != "-i" ]; then
+if [[ "$1" != "-appMeta"* ]] && [[ "$1" != "-i"* ]]; then
     echoUsage
     exit 1
 fi
 
-if [ "$1" == "-appMeta" ]; then
+if [[ "$1" == "-appMeta"* ]]; then
 	# Note: format is: (1)||(2)||(3)||(4)||(5)
 	#   All must be present, even if null!
 	# (1) - App Description (user-friendly name of the App)
@@ -23,24 +23,25 @@ if [ "$1" == "-appMeta" ]; then
 	# (5) - Proof Paths - Can be 1 path or multiple. If multiple paths, separate each by single pipe (|) - if ANY of the paths exist, it's proof app is already installed.
 
     # Get Localized Description
-	if [ "$2" == "tr" ]; then
+	if [ "$1" == "-appMeta tr" ]; then
         desc="[tr]Chrome"
-	elif [ "$2" == "ru" ]; then
+	elif [ "$1" == "-appMeta ru" ]; then
 		desc="[ru]Chrome"
 	else
 		desc="Chrome"
     fi
     
-	echo "$desc||https://dl.google.com/chrome/mac/stable/GGRO/googlechrome.dmg||user||/Applications/Google Chrome.app"
+	echo "$desc||https://dl.google.com/chrome/mac/stable/GGRO/googlechrome.dmg||googlechrome.dmg||user||/Applications/Google Chrome.app"
     exit 0
 fi
 
 # Install
-if [ "$1" == "-i" ]; then
-	if [ ! -e "$2/googlechrome.dmg" ]; then
-		echo "Chrome is missing from sourceFolder: $2"
+if [[ "$1" == "-i"* ]]; then
+    sourceFolder=${1:3} # Strip off first 3 characters from $1 (-i )
+	if [ ! -e "$sourceFolder/googlechrome.dmg" ]; then
+		echo "Chrome is missing from sourceFolder: $sourceFolder"
 	else
-		hdiutil mount -nobrowse -quiet $2/googlechrome.dmg
+		hdiutil mount -nobrowse -quiet $sourceFolder/googlechrome.dmg
 		cp -R "/Volumes/Google Chrome/Google Chrome.app" /Applications
 		hdiutil unmount -quiet "/Volumes/Google Chrome"
 		echo "Installed Chrome"
